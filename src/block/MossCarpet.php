36721324c8aaa2453deda3d9f1bdf6934cdd0b8f
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-24 20:44:14 +0700

    Create MossCarpet.php

---
+++ src/block/MossCarpet.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\block;
+
+use pocketmine\block\VanillaBlocks;
+use pocketmine\math\Facing;
+
+class MossCarpet extends Block{
+
+    public function __construct(){
+        parent::__construct(BlockTypeIds::MOSS_CARPET, 0, "Moss Carpet");
+    }
+
+    public function getBlastResistance(): float{
+        return 0.1;
+    }
+
+    public function getHardness(): float{
+        return 0.1;
+    }
+
+    public function onInteract(Block $block): void{
+        $this->growVines();
+    }
+
+    private function growVines(): void{
+        $world = $this->getPosition()->getWorld();
+        foreach(Facing::HORIZONTAL as $face){
+            $neighbor = $this->getSide($face);
+            if($neighbor->isSolid()){
+                $vinePosition = $neighbor->getPosition()->getSide(Facing::DOWN);
+                $world->setBlock($vinePosition, new PaleMossVine());
+            }
+        }
+    }
+}
